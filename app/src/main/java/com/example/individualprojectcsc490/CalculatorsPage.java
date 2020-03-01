c3e491cba2068393a8a11adb1febd0534d1fba32
TheXenonPhoenix <majorgamerkevin@gmail.com>
2020-03-01 12:59:20 -0600

    created Calculators page

---
+++ app/src/main/java/com/example/individualprojectcsc490/CalculatorsPage.java
@@ -0,0 +1,14 @@
+package com.example.individualprojectcsc490;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CalculatorsPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculators_page);
+    }
+}
