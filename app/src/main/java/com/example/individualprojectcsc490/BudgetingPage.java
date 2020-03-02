e507a350cfd60871c0c81a2a733a8758949842b0
kevinphoenix16 <kevinphoenix16@augustana.edu>
2020-03-02 08:10:33 -0600

    added budgeting page

---
+++ app/src/main/java/com/example/individualprojectcsc490/BudgetingPage.java
@@ -0,0 +1,14 @@
+package com.example.individualprojectcsc490;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BudgetingPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_budgeting_page);
+    }
+}
