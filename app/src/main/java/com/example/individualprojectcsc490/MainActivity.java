ea6110106eef1bc6d16d4dbdf5fe6acfd205bd0d
kevinphoenix16 <kevinphoenix16@augustana.edu>
2020-03-02 08:16:30 -0600

    added budgeting page navigation

--- app/src/main/java/com/example/individualprojectcsc490/MainActivity.java
+++ app/src/main/java/com/example/individualprojectcsc490/MainActivity.java
@@ -10,12 +10,14 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     private Button calculatorsButton;
+    private Button budgetingButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Calculators Button
         calculatorsButton = (Button) findViewById(R.id.calculatorsButton);
         calculatorsButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,10 +25,26 @@ public class MainActivity extends AppCompatActivity {
                 openCalculatorsPage();
             }
         });
+
+        //Budgeting Button
+        budgetingButton = (Button) findViewById(R.id.budgetingButton);
+        budgetingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openBudgetingPage();
+            }
+        });
     }
 
+    //Switches to CalculatorsPage Activity
     public void openCalculatorsPage(){
         Intent intent = new Intent(this, CalculatorsPage.class);
+        startActivity(intent);
+    }
+
+    //Switches to BudgetingPage Activity
+    public void openBudgetingPage(){
+        Intent intent = new Intent(this, BudgetingPage.class);
         startActivity(intent);
     }
 }
