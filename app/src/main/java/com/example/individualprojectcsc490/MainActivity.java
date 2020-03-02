261e8d9564409020a6c7b162b4adb46bf603c905
kevinphoenix16 <kevinphoenix16@augustana.edu>
2020-03-02 08:01:58 -0600

    added navigation to calculators page

--- app/src/main/java/com/example/individualprojectcsc490/MainActivity.java
+++ app/src/main/java/com/example/individualprojectcsc490/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.individualprojectcsc490;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button calculatorsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        calculatorsButton = (Button) findViewById(R.id.calculatorsButton);
+        calculatorsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCalculatorsPage();
+            }
+        });
+    }
+
+    public void openCalculatorsPage(){
+        Intent intent = new Intent(this, CalculatorsPage.class);
+        startActivity(intent);
     }
 }
