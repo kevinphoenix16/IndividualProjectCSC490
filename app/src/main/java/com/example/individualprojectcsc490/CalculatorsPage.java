ad51138ad9196c7fd4e89985f4aa766c4d63c09d
kevinphoenix16 <kevinphoenix16@augustana.edu>
2020-03-04 12:06:40 -0600

    created FIRE Number Calculator Page

--- app/src/main/java/com/example/individualprojectcsc490/CalculatorsPage.java
+++ app/src/main/java/com/example/individualprojectcsc490/CalculatorsPage.java
@@ -2,13 +2,35 @@ package com.example.individualprojectcsc490;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CalculatorsPage extends AppCompatActivity {
+
+    private Button FIRENumberButton;
+    private Button savingsRateButton;
+    private Button averageSpendingButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculators_page);
+
+        //FIRENumber Button
+        FIRENumberButton = (Button) findViewById(R.id.FIRENumberButton);
+        FIRENumberButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openFIRENumberPage();
+            }
+        });
+    }
+
+    //Switches to FIRENumberPage Activity
+    public void openFIRENumberPage(){
+        Intent intent = new Intent(this, CalculatorsPage.class);
+        startActivity(intent);
     }
 }
