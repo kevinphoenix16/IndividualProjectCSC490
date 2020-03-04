0fd9ead7bedea3d0338a5b8882f76ccb35fa71cb
kevinphoenix16 <kevinphoenix16@augustana.edu>
2020-03-04 12:19:05 -0600

    created Savings Rate Calculator Page

---
+++ app/src/main/java/com/example/individualprojectcsc490/Calculators/SavingsRatePage.java
@@ -0,0 +1,16 @@
+package com.example.individualprojectcsc490.Calculators;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.individualprojectcsc490.R;
+
+public class SavingsRatePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_savings_rate_page);
+    }
+}
--- app/src/main/java/com/example/individualprojectcsc490/CalculatorsPage.java
+++ app/src/main/java/com/example/individualprojectcsc490/CalculatorsPage.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.individualprojectcsc490.Calculators.*;
+
 public class CalculatorsPage extends AppCompatActivity {
 
     private Button FIRENumberButton;
@@ -26,11 +28,26 @@ public class CalculatorsPage extends AppCompatActivity {
                 openFIRENumberPage();
             }
         });
+
+        //SavingsRate Button
+        savingsRateButton = (Button) findViewById(R.id.savingsRateButton);
+        savingsRateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSavingsRatePage();
+            }
+        });
     }
 
     //Switches to FIRENumberPage Activity
     public void openFIRENumberPage(){
-        Intent intent = new Intent(this, CalculatorsPage.class);
+        Intent intent = new Intent(this, FIRENumberPage.class);
+        startActivity(intent);
+    }
+
+    //Switches to SavingsRatePage Activity
+    public void openSavingsRatePage(){
+        Intent intent = new Intent(this, SavingsRatePage.class);
         startActivity(intent);
     }
 }
