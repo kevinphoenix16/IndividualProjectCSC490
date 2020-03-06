75ef5fa800bb7675e7342eb9406dae74ef9f4cd1
kevinphoenix16 <kevinphoenix16@augustana.edu>
2020-03-06 15:08:15 -0600

    added drop-down to choose between annual and monthly payments with functioning calculations

--- app/src/main/java/com/example/individualprojectcsc490/Calculators/AverageSpendingPage.java
+++ app/src/main/java/com/example/individualprojectcsc490/Calculators/AverageSpendingPage.java
@@ -4,16 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.example.individualprojectcsc490.R;
 
 public class AverageSpendingPage extends AppCompatActivity {
 
-    private final int ANNUAL = 1;
+    private final int ANNUALLY = 1;
     private final int MONTHLY = 12;
 
+    private Spinner paymentPeriodSpinner;
     private Button compute;
     private TextView averageSpendingValue;
 
@@ -29,29 +32,45 @@ public class AverageSpendingPage extends AppCompatActivity {
         setContentView(R.layout.activity_average_spending_page);
 
         setupUI();
+        setupSpinner();
 
         //Compute Button
         compute.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 /* TO-DO:
-                * Decide if using annual or monthly
-                * InProgress - Get all values from text fields
-                * Combine all values with multiplier
-                * Done - Update averageSpendingValue
-                */
+                 * InProgress - Get all values from text fields
+                 * Combine all values with multiplier
+                 * Done - Update averageSpendingValue
+                 */
                 double subTotal = sumCategories();
-                averageSpendingValue.setText(Double.toString(subTotal));
+                String paymentPeriod = paymentPeriodSpinner.getSelectedItem().toString();
+
+                if(paymentPeriod.equals("Monthly")){
+                    averageSpendingValue.setText(Double.toString(subTotal * MONTHLY));
+                } else if(paymentPeriod.equals("Annually")) {
+                    averageSpendingValue.setText(Double.toString(subTotal * ANNUALLY));
+                }
             }
         });
     }
 
     //Sets up local variables
     private void setupUI() {
+        paymentPeriodSpinner = (Spinner) findViewById(R.id.paymentPeriodSpinner);
         compute              = (Button) findViewById(R.id.computeButton);
+        averageSpendingValue = (TextView) findViewById(R.id.averageSpendingValue);
+
         foodFieldValue       = (TextView) findViewById(R.id.foodFieldValue);
         housingFieldValue    = (TextView) findViewById(R.id.housingFieldValue);
-        averageSpendingValue = (TextView) findViewById(R.id.averageSpendingValue);
+    }
+
+    //Sets up the drop-down Spinner container
+    private void setupSpinner() {
+        ArrayAdapter<String> paymentPeriodAdapter = new ArrayAdapter<String>(AverageSpendingPage.this,
+                android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.paymentPeriodOptions));
+        paymentPeriodAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        paymentPeriodSpinner.setAdapter(paymentPeriodAdapter);
     }
 
     //Adds up all categories of spending
