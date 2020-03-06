0bc0e4f1e867f54c6427ae27ebc2c7a6197c11ba
kevinphoenix16 <kevinphoenix16@augustana.edu>
2020-03-06 13:53:05 -0600

    added first category to be calculated on average spending with working calculation functionality

--- app/src/main/java/com/example/individualprojectcsc490/Calculators/AverageSpendingPage.java
+++ app/src/main/java/com/example/individualprojectcsc490/Calculators/AverageSpendingPage.java
@@ -3,14 +3,63 @@ package com.example.individualprojectcsc490.Calculators;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.individualprojectcsc490.R;
 
 public class AverageSpendingPage extends AppCompatActivity {
 
+    private final int ANNUAL = 1;
+    private final int MONTHLY = 12;
+
+    private Button compute;
+    private TextView averageSpendingValue;
+
+    private TextView foodFieldValue;
+
+
+    private double foodValue;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_average_spending_page);
+
+        setupUI();
+
+        //Compute Button
+        compute.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /* TO-DO:
+                * Decide if using annual or monthly
+                * InProgress - Get all values from text fields
+                * Combine all values with multiplier
+                * Done - Update averageSpendingValue
+                */
+                double subTotal = sumCategories();
+
+                averageSpendingValue.setText(Double.toString(subTotal));
+            }
+        });
+
+
     }
+
+    //Sets up local variables
+    private void setupUI() {
+        compute              = (Button) findViewById(R.id.computeButton);
+        foodFieldValue       = (TextView) findViewById(R.id.foodFieldValue);
+        averageSpendingValue = (TextView) findViewById(R.id.averageSpendingValue);
+    }
+
+    //Adds up all categories of spending
+    private double sumCategories(){
+        foodValue = Double.parseDouble(foodFieldValue.getText().toString());
+
+        return foodValue;
+    }
+
 }
