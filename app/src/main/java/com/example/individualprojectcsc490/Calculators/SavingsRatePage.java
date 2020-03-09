818e0a13fecbf5b2c77f4943f229119bc3fc8ec0
kevinphoenix16 <kevinphoenix16@augustana.edu>
2020-03-09 13:38:37 -0500

    setup UI on retire early calculator

--- app/src/main/java/com/example/individualprojectcsc490/Calculators/SavingsRatePage.java
+++ app/src/main/java/com/example/individualprojectcsc490/Calculators/SavingsRatePage.java
@@ -5,9 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.example.individualprojectcsc490.R;
